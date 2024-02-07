d28e9986735df2d8fe9546ae330e00909052292c
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-02-07 22:12:12 +0530

    controlled component

--- Two-sheryians/src/components/Form.jsx
+++ Two-sheryians/src/components/Form.jsx
@@ -1,22 +1,56 @@
-import React,{ useRef }  from 'react'
+//using ref 
 
+// import React,{ useRef }  from 'react'
+
+
+// function Form() {
+//     const name = useRef(null);
+//     const age = useRef(null)
+//     const handleSubmit = (event)=>{
+// event.preventDefault()
+// console.log(name.current.value, age.current.value)
+//     }
+//   return (
+//     <>
+//     <form action="" onSubmit={handleSubmit}>
+// <input type="text"  ref={name} placeholder='name'/>
+// <input type="age"  ref={age} placeholder='age'/>
+// <input type="submit" />
+//     </form>
+
+
+    
+//     </>
+//   )
+// }
+
+// export default Form
+
+
+
+
+
+
+
+//using controlled components
+
+import React,{useState} from 'react'
 
 function Form() {
-    const name = useRef(null);
-    const age = useRef(null)
-    const handleSubmit = (event)=>{
+    const [val, setVal] = useState({name:"",email:""});
+    const handleSubmit =(event)=>{
 event.preventDefault()
-console.log(name.current.value, age.current.value)
+console.log(val)
     }
   return (
-    <>
-    <form action="" onSubmit={handleSubmit}>
-<input type="text"  ref={name} placeholder='name'/>
-<input type="age"  ref={age} placeholder='age'/>
+   <>
+<form action="" onSubmit={handleSubmit}>
+<input type="text" onChange={(event)=>setVal({...val,name:event.target.value})} placeholder='name'/>
+<input type="text" onChange={(event)=>setVal({...val,email:event.target.value})} placeholder='email'/>
 <input type="submit" />
-    </form>
-    
-    </>
+</form>
+
+   </>
   )
 }
 
